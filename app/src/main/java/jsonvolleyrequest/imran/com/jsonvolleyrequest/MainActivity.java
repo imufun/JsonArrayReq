2b9c4611ac50cb4f48519e3204c67fb8e8900a2e
imran <imufun1@gmail.com>
2016-07-21 18:05:33 +0600

    dfs

--- app/src/main/java/jsonvolleyrequest/imran/com/jsonvolleyrequest/MainActivity.java
+++ app/src/main/java/jsonvolleyrequest/imran/com/jsonvolleyrequest/MainActivity.java
@@ -3,20 +3,24 @@ package jsonvolleyrequest.imran.com.jsonvolleyrequest;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     private String UrlJsonObj = "http://api.androidhive.info/volley/person_object.json";
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog mDialog;
     private TextView mTextView;
 
-    private String textResponse;
+    private String jsonResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,23 +80,112 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Method to make json object request where json response starts wtih {
+     */
     private void makeJsonObjectRequest() {
-        showDialog();
-        //Toast.makeText(MainActivity.this, "sfd", Toast.LENGTH_LONG).show();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, UrlJsonObj, null, new Response.Listener<JSONObject>() {
+        showDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest( Request.Method.GET ,UrlJsonObj, new Response.Listener<JSONObject>() {
+
             @Override
             public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
 
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    String name = response.getString("name");
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = phone.getString("home");
+                    String mobile = phone.getString("mobile");
+
+                    jsonResponse = "";
+                    jsonResponse += "Name: " + name + "\n\n";
+                    jsonResponse += "Email: " + email + "\n\n";
+                    jsonResponse += "Home: " + home + "\n\n";
+                    jsonResponse += "Mobile: " + mobile + "\n\n";
+
+                    mTextView.setText(jsonResponse);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hideDialog();
             }
         }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hideDialog();
             }
-        }
-    });
+        });
 
+        // Adding request to request queue
+       AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    /**
+     * Method to make json object request where json response starts wtih {
+     */
+//    private void makeJsonObjectRequest() {
+//
+//        showDialog();
+//
+//        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+//                UrlJsonObj, null, new Response.Listener<JSONObject>() {
+//
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//
+//                try {
+//
+//
+//                } catch (JSONException e) {
+//
+//                }
+//
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+//
+//
+//    }
+
+
+//    private void makeJsonObjectRequest() {
+//        showDialog();
+//        //Toast.makeText(MainActivity.this, "sfd", Toast.LENGTH_LONG).show();
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, UrlJsonObj, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//            }
+//
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        }
+//
+//    });
     private void makeJsonArrayRequest() {
     }
 }
